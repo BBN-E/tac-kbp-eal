8ef46b3a52285f1bf4f35f6f7e247eb8b5384b75
Ryan Gabbard <rgabbard@bbn.com>
2014-06-10 15:16:38 -0400

    Bugfix: neutralizing realis could result in inconsistent assessments

--- kbp-events2014/src/main/java/com/bbn/kbp/events2014/transformers/MakeAllRealisActual.java
+++ kbp-events2014/src/main/java/com/bbn/kbp/events2014/transformers/MakeAllRealisActual.java
@@ -5,6 +5,10 @@ import com.bbn.kbp.events2014.*;
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+import java.util.Set;
 
 public final class MakeAllRealisActual {
     private MakeAllRealisActual() {
@@ -15,19 +19,32 @@ public final class MakeAllRealisActual {
         return new Function<AnswerKey, AnswerKey>() {
             @Override
             public AnswerKey apply(AnswerKey input) {
-                final ImmutableList.Builder<Response> newUnannotated = ImmutableList.builder();
-
-                for (final Response response : input.unannotatedResponses()) {
-                    newUnannotated.add(response.copyWithSwappedRealis(KBPRealis.Actual));
-                }
-
-                final ImmutableList.Builder<AssessedResponse> newAssessed = ImmutableList.builder();
+                // seenResponses is to ensure we don't end up with two different
+                // assessments for the same response, or have the same response
+                // as both assessed and unassessed
+                final Set<Response> seenResponses = Sets.newHashSet();
+                final ImmutableSet.Builder<AssessedResponse> newAssessed = ImmutableSet.builder();
 
                 for (final AssessedResponse response : input.annotatedResponses()) {
-                    newAssessed.add(AssessedResponse.from(
-                            response.response().copyWithSwappedRealis(KBPRealis.Actual),
+                    final Response neutralizedResponse = response.response().copyWithSwappedRealis(KBPRealis.Actual);
+                    if (!seenResponses.contains(neutralizedResponse)) {
+                        newAssessed.add(AssessedResponse.from(neutralizedResponse,
                             response.assessment().copyWithModifiedRealisAssessment(Optional.of(KBPRealis.Actual))));
+                    }
+                    seenResponses.add(neutralizedResponse);
                 }
+
+
+                final ImmutableSet.Builder<Response> newUnannotated = ImmutableSet.builder();
+
+                for (final Response response : input.unannotatedResponses()) {
+                    final Response neutralizedResponse = response.copyWithSwappedRealis(KBPRealis.Actual);
+                    if (!seenResponses.contains(neutralizedResponse)) {
+                        newUnannotated.add(neutralizedResponse);
+                    }
+                    seenResponses.add(neutralizedResponse);
+                }
+
 
                 return AnswerKey.from(input.docId(), newAssessed.build(), newUnannotated.build());
             }
@@ -38,11 +55,11 @@ public final class MakeAllRealisActual {
         return new Function<SystemOutput, SystemOutput>() {
             @Override
             public SystemOutput apply(SystemOutput input) {
-                final ImmutableList.Builder<Scored<Response>> newResponses = ImmutableList.builder();
+                final ImmutableSet.Builder<Scored<Response>> newResponses = ImmutableSet.builder();
 
-                for (final Scored<Response> response : input.scoredResponses()) {
-                    newResponses.add(Scored.from(response.item().copyWithSwappedRealis(KBPRealis.Actual),
-                            response.score()));
+                for (final Scored<Response> assessedResponse : input.scoredResponses()) {
+                    newResponses.add(Scored.from(assessedResponse.item().copyWithSwappedRealis(KBPRealis.Actual),
+                            assessedResponse.score()));
                 }
 
                 return SystemOutput.from(input.docId(), newResponses.build());
