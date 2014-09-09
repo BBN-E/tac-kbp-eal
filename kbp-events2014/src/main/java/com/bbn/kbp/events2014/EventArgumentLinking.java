78ecb47fa201f9e19ca8f6d0b3c3a5d1e39c4894
Ryan Gabbard <rgabbard@bbn.com>
2014-09-09 16:39:43 -0400

    Creating EventArgumentLinkings from ResponseLinking + coref

--- kbp-events2014/src/main/java/com/bbn/kbp/events2014/EventArgumentLinking.java
+++ kbp-events2014/src/main/java/com/bbn/kbp/events2014/EventArgumentLinking.java
@@ -34,6 +34,72 @@ public final class EventArgumentLinking {
         return incomplete;
     }
 
+    /** Converts a {@link ResponseLinking} to an {@link EventArgumentLinking} using
+     * a {@link CorefAnnotation} to canonicalize the responses.  If the canonicalization
+     * is inconsistent with the response linking, an {@link java.lang.IllegalArgumentException}
+     * will be thrown.
+     */
+    public static EventArgumentLinking createFromResponseLinking(ResponseLinking responseLinking,
+                                                                 CorefAnnotation corefAnnotation)
+    {
+        checkArgument(corefAnnotation.docId() == responseLinking.docID());
+
+
+        final ImmutableMultimap<TypeRoleFillerRealis, Response> canonicalToResponses =
+                Multimaps.index(responseLinking.allResponses(),
+                        TypeRoleFillerRealis.extractFromSystemResponse(
+                                corefAnnotation.strictCASNormalizerFunction()));
+
+        final Multimap<Response, TypeRoleFillerRealis> responsesToCanonical =
+                canonicalToResponses.inverse();
+
+        final ImmutableSet.Builder<TypeRoleFillerRealisSet> coreffedArgs = ImmutableSet.builder();
+        for (final ResponseSet responseSet : responseLinking.responseSets()) {
+            coreffedArgs.add(TypeRoleFillerRealisSet.from(
+                    canonicalizeResponseSet(responseSet.asSet(),
+                        canonicalToResponses, responsesToCanonical)));
+        }
+        final ImmutableSet<TypeRoleFillerRealis> incompleteResponses = canonicalizeResponseSet(
+                responseLinking.incompleteResponses(), canonicalToResponses, responsesToCanonical);
+
+        return new EventArgumentLinking(responseLinking.docID(), coreffedArgs.build(),
+                incompleteResponses);
+    }
+
+    /** Maps a {@code Set<Response>} to a {@code Set<TypeRoleFillerRealis>} which
+     is supported by exactly the same responses.  If any response supporting
+     a TRFR is present in {@code responseGroup}, that TRFR will appear in the
+     returned set. However, if such a TRFR is supported by responses but within
+     and outside {@code responseSet}, an {@link java.lang.IllegalArgumentException} is thrown.
+     */
+    private static ImmutableSet<TypeRoleFillerRealis> canonicalizeResponseSet(
+            Set<Response> responseGroup,
+            Multimap<TypeRoleFillerRealis, Response> canonicalToResponses,
+            Multimap<Response, TypeRoleFillerRealis> responsesToCanonical)
+    {
+        final ImmutableSet.Builder<TypeRoleFillerRealis> canonicalizationsWithResponsesInGroupBuilder =
+                ImmutableSet.builder();
+        for (final Response response : responseGroup) {
+            for (final TypeRoleFillerRealis canonicalization : responsesToCanonical.get(response)) {
+                canonicalizationsWithResponsesInGroupBuilder.add(canonicalization);
+            }
+        }
+
+        final ImmutableSet<TypeRoleFillerRealis> ret =
+                canonicalizationsWithResponsesInGroupBuilder.build();
+
+        // sanity check
+        for (final TypeRoleFillerRealis canonicalization : ret) {
+            for (final Response response : canonicalToResponses.get(canonicalization)) {
+                if (!responseGroup.contains(response)) {
+                    throw new IllegalArgumentException("Response linking inconsistent with coreference annotation:");
+                }
+            }
+        }
+
+        return ret;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hashCode(docID, coreffedArgSets, incomplete);
--- kbp-events2014/src/main/java/com/bbn/kbp/events2014/TypeRoleFillerRealisSet.java
+++ kbp-events2014/src/main/java/com/bbn/kbp/events2014/TypeRoleFillerRealisSet.java
@@ -7,25 +7,23 @@ import com.google.common.collect.ImmutableSet;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class TypeRoleFillerRealisSet {
-    private final Symbol docID;
     private final ImmutableSet<TypeRoleFillerRealis> coreffedArgs;
 
-    private TypeRoleFillerRealisSet(final Symbol docID, Iterable<TypeRoleFillerRealis> coreffedArgs) {
-        this.docID = checkNotNull(docID);
+    private TypeRoleFillerRealisSet(Iterable<TypeRoleFillerRealis> coreffedArgs) {
         this.coreffedArgs = ImmutableSet.copyOf(coreffedArgs);
-    }
-
-    public Symbol docID() {
-        return docID;
     }
 
     public ImmutableSet<TypeRoleFillerRealis> asSet() {
         return coreffedArgs;
     }
 
+    public static TypeRoleFillerRealisSet from(Iterable<TypeRoleFillerRealis> items) {
+        return new TypeRoleFillerRealisSet(items);
+    }
+
     @Override
     public int hashCode() {
-        return Objects.hashCode(docID, coreffedArgs);
+        return Objects.hashCode(coreffedArgs);
     }
 
     @Override
@@ -37,6 +35,6 @@ public final class TypeRoleFillerRealisSet {
             return false;
         }
         final TypeRoleFillerRealisSet other = (TypeRoleFillerRealisSet) obj;
-        return Objects.equal(this.docID, other.docID) && Objects.equal(this.coreffedArgs, other.coreffedArgs);
+        return Objects.equal(this.coreffedArgs, other.coreffedArgs);
     }
 }
