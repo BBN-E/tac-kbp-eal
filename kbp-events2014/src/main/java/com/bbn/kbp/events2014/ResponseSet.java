d5669b98fcebc2e20cf1b0c874acef7d26d24f02
Ryan Gabbard <rgabbard@bbn.com>
2014-09-09 15:11:50 -0400

    Makes response set iterable

--- kbp-events2014/src/main/java/com/bbn/kbp/events2014/ResponseSet.java
+++ kbp-events2014/src/main/java/com/bbn/kbp/events2014/ResponseSet.java
@@ -6,11 +6,13 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Ordering;
 
+import java.util.Iterator;
+
 /**
  * Represents a set of responses.  The default comparison method is to
  * do a lexicographical sort of the unique IDs of the (sorted) responses.
  */
-public final class ResponseSet implements Comparable<ResponseSet> {
+public final class ResponseSet implements Comparable<ResponseSet>, Iterable<Response> {
     private final ImmutableSet<Response> responses;
     private final int cachedHashCode;
 
@@ -64,5 +66,10 @@ public final class ResponseSet implements Comparable<ResponseSet> {
                 .lexicographical()
                 .onResultOf(asSetFunction())
                 .compare(this, o);
+    }
+
+    @Override
+    public Iterator<Response> iterator() {
+        return asSet().iterator();
     }
 }
