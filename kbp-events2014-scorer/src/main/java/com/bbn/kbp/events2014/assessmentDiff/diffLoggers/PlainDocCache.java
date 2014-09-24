cf21004f18cfa453e5ba9d0f27433f78d8f36be0
Ryan Gabbard <rgabbard@bbn.com>
2014-09-24 17:27:21 -0400

    A little cleanup to PlainDocCache

--- kbp-events2014-scorer/src/main/java/com/bbn/kbp/events2014/assessmentDiff/diffLoggers/PlainDocCache.java
+++ kbp-events2014-scorer/src/main/java/com/bbn/kbp/events2014/assessmentDiff/diffLoggers/PlainDocCache.java
@@ -1,72 +1,29 @@
 package com.bbn.kbp.events2014.assessmentDiff.diffLoggers;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-import com.bbn.bue.common.files.FileUtils;
 import com.bbn.bue.common.symbols.Symbol;
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 
-public class PlainDocCache {
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public final class PlainDocCache {
+    private final Map<Symbol, File> docIDToFileMap;
     
-    //private final LoadingCache<Symbol, String> innerCache;
-    private final Map<Symbol, File> tempMapHack; // get rid of this eventually
-//    
-//    private PlainDocCache(final LoadingCache<Symbol, String> innerCache) {
-//        this.innerCache = checkNotNull(innerCache);
-//    }
-    
-    public PlainDocCache(final Map<Symbol, File> fileMap) {
-        this.tempMapHack = fileMap;
+    private PlainDocCache(final Map<Symbol, File> fileMap) {
+        docIDToFileMap = ImmutableMap.copyOf(fileMap);
     }
+
+    public static PlainDocCache createFromDocIDToFileMap(final Map<Symbol, File> fileMap) {
+        return new PlainDocCache(fileMap);
+    }
+
+    // despite the name, this does not appear to actually do any caching? I've cleaned it up
+    // a little, but I'm not sure this is actually even used, so I won't bother with it
+    // more for now...
     public String getPlainDoc(final Symbol docid) throws IOException {
-//        try {
-//            return innerCache.get(docid);
-//        }
-//        catch (final ExecutionException e) {
-//            if (e.getCause() instanceof IOException) {
-//                throw (IOException)e.getCause();
-//            } else {
-//                throw new RuntimeException(e);
-//            }
-//        }
-        try {
-            String plainDoc = Files.toString(this.tempMapHack.get(docid), Charsets.UTF_8);
-            // /nfs/mercury-04/u10/kbp/pilot/all.list
-            return plainDoc;
-        }
-        catch (IOException e) {
-            throw new IOException(e);
-        }
-    }
-
-    public static Builder createFromDocIdMap(final Map<Symbol, File> docidToTextFile) {
-        return Builder.fromDocIdMap(docidToTextFile);
-    }
-
-    public static class Builder {
-        private Builder() {}
-
-//        public String build() throws IOException {
-//            return new PlainDocCache(
-//                    CacheBuilder.newBuilder()
-//                    .maximumSize(maxElements>=0?maxElements:10)
-//                    .<Symbol,String>build(DocIDMapCacheLoader.from(
-//                            docidMap,
-//                            loader != null ? loader : SerifXMLLoader.fromStandardACETypes())));
-//        }
-
-        private static Builder fromDocIdMap(final Map<Symbol, File> docidToPlainText) {
-            final Builder ret = new Builder();
-            ret.docidMap = ImmutableMap.copyOf(docidToPlainText);
-            return ret;
-        }
-    
-        private Map<Symbol, File> docidMap = null;
-        //private int maxElements = 20;
+        return Files.toString(this.docIDToFileMap.get(docid), Charsets.UTF_8);
     }
 }
