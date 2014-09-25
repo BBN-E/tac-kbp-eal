c096c59d4a4fbcff55791bf26fc39025f4e0e5ed
Yee Seng Chan <ychan@bbn.com>
2014-09-25 11:33:10 -0400

    sanity checks to SameEventTypeLinker: only accept Response with ACTUAL,OTHER Realis ; only accept Response that are found in the AnswerKey pool (can be either assessed or not assessed)

--- kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/ApplyLinkingStrategy.java
+++ kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/ApplyLinkingStrategy.java
@@ -1,8 +1,6 @@
 package com.bbn.kbp.events2014.bin;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +8,7 @@ import org.slf4j.LoggerFactory;
 import com.bbn.bue.common.parameters.Parameters;
 import com.bbn.bue.common.symbols.Symbol;
 import com.bbn.kbp.events2014.AnswerKey;
-import com.bbn.kbp.events2014.AssessedResponse;
-import com.bbn.kbp.events2014.CorefAnnotation;
-import com.bbn.kbp.events2014.EventArgumentLinking;
+import com.bbn.kbp.events2014.KBPRealis;
 import com.bbn.kbp.events2014.Response;
 import com.bbn.kbp.events2014.ResponseLinking;
 import com.bbn.kbp.events2014.ResponseSet;
@@ -22,11 +18,8 @@ import com.bbn.kbp.events2014.io.AssessmentSpecFormats;
 import com.bbn.kbp.events2014.io.LinkingSpecFormats;
 import com.bbn.kbp.events2014.io.LinkingStore;
 import com.bbn.kbp.events2014.io.SystemOutputStore;
-import com.bbn.kbp.events2014.linking.EventArgumentLinkingAligner.InconsistentLinkingException;
-import com.bbn.kbp.events2014.linking.ExactMatchEventArgumentLinkingAligner;
 import com.bbn.kbp.events2014.linking.LinkingStrategy;
 import com.bbn.kbp.events2014.linking.SameEventTypeLinker;
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
 
 public final class ApplyLinkingStrategy {
@@ -37,8 +30,9 @@ public final class ApplyLinkingStrategy {
 				 "usage: applyLinkingStrategy parameterFile\n" +
 				 "Parameter files are lines of key : value pairs\n" +
 				 "Parameters:\n" +
-				 "\targumentSystemStore: the system output to link\n" +
-				 "\tlinkingSystemStore: destination directory to write out the produced links\n");
+				 "\targumentAnnotationStore: directory containing assessed EA tuples\n" +
+				 "\targumentSystemStore: directory containing system EA tuples to link\n" +
+				 "\tlinkingSystemStore: destination directory to write out the linked system EA tuples\n");
 		 System.exit(1);
 	 }
 	 
@@ -51,34 +45,52 @@ public final class ApplyLinkingStrategy {
 			 final Parameters params = Parameters.loadSerifStyle(new File(argv[0]));
 			 log.info(params.dump());
 			 
-			 final File systemOutputStoreDir = params.getExistingDirectory("argumentSystemStore");	// system EA tuples
-			 
 			 final AssessmentSpecFormats.Format fileFormat = AssessmentSpecFormats.Format.KBP2014;
-	         
-			 // whether it is KBP2014 or KBP2015 doesn't really matter, because it is only used when I call the write method, which is not used here
-			 final SystemOutputStore systemOutputStore = AssessmentSpecFormats.openSystemOutputStore(systemOutputStoreDir, fileFormat);
-			 final ImmutableSet<Symbol> docIDs = systemOutputStore.docIDs();
 			 
-			 final File systemLinkingStoreDir = params.getExistingDirectory("linkingSystemStore");	// destination directory to write to
-			 final LinkingStore linkingStore = LinkingSpecFormats.openOrCreateLinkingStore(systemLinkingStoreDir);
+			 // produced by annotators after they have assessed EA tuples
+			 // we'll use this as sanity check: discard any system response not found in overall pool
+			 final File argumentAnnotationStoreDir = params.getExistingDirectory("argumentAnnotationStore");
+			 final AnnotationStore argumentAnnotationStore = AssessmentSpecFormats.openAnnotationStore(argumentAnnotationStoreDir, fileFormat);
+			 // DirectoryAnnotationStore implements AnnotationStore
+			 // fileFormat is only used in argumentKeyStore.write method
+			 
+			 final File argumentSystemStoreDir = params.getExistingDirectory("argumentSystemStore");	// system EA tuples	         
+			 // whether it is KBP2014 or KBP2015 doesn't really matter, because it is only used when I call the write method, which is not used here
+			 final SystemOutputStore argumentSystemStore = AssessmentSpecFormats.openSystemOutputStore(argumentSystemStoreDir, fileFormat);
+			 final ImmutableSet<Symbol> docIDs = argumentSystemStore.docIDs();
+			 
+			 final File linkingSystemStoreDir = params.getExistingDirectory("linkingSystemStore");	// destination directory to write to
+			 final LinkingStore linkingSystemStore = LinkingSpecFormats.openOrCreateLinkingStore(linkingSystemStoreDir);
 		     
 			 // default to SameEventTypeLinker for now. We could parameterize this in future when there's multiple strategies.
-			 LinkingStrategy linkingStrategy = SameEventTypeLinker.create();
+			 LinkingStrategy linkingStrategy = SameEventTypeLinker.create(ImmutableSet.of(KBPRealis.Actual, KBPRealis.Other));
 			 
 			 for(final Symbol docID : docIDs) {
-				 final SystemOutput docOutput = systemOutputStore.read(docID);
+				 final AnswerKey answerKey = argumentAnnotationStore.read(docID);
+				 
+				 final SystemOutput docOutput = argumentSystemStore.read(docID);
 				 // Symbol docId, Set<Response> responses , Map<Response, Double> confidences
 				 
 				 log.info("For document {} got {} responses", docID, docOutput.size());
 				 
-				 final ResponseLinking responseLinking = linkingStrategy.linkResponses(docOutput);
+				 // we will do a sanity check: filter Responses in docOutput against answerKey.allResponses()
+				 // TODO: answerKey.allResponses() include unannotated Responses. Would it be possible for a system response to be unannotated?
+				 final ResponseLinking responseLinking = linkingStrategy.linkResponses(docOutput, answerKey);
 				 // Symbol docId, Set<ResponseSet> responseSets , Set<Response> incompleteResponses (which will be empty)
 				 
-				 linkingStore.write(responseLinking);
+				 linkingSystemStore.write(responseLinking);
 				 // for each ResponseSet , writes out uniqueIdFunction() of each Response on the same line
 				 // the id of each Response is the cachedSHA1Hash, which is everything about a Response: type, role, CAS, realis, basefiller, predicate justifications, etc.
+				 
+				 
+				 for (final ResponseSet responseSet : responseLinking.responseSets()) {
+					 for(final Response r : responseSet.asSet()) {
+						 System.out.println(r.uniqueIdentifier() + " " + r);
+					 }
+				 }
+				 
 			 }
-			 linkingStore.close();
+			 linkingSystemStore.close();
 				    
 		 } catch (Throwable t) {
 			 t.printStackTrace();
