d9bb7daf276ded532b2105d896c5a11bc3d24a5e
Ryan Gabbard <rgabbard@bbn.com>
2014-10-06 13:29:02 -0400

    Allow restricting which docIDs are imported

--- kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/ImportSystemOutputToAnnotationStore.java
+++ kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/ImportSystemOutputToAnnotationStore.java
@@ -2,19 +2,24 @@ package com.bbn.kbp.events2014.bin;
 
 import com.bbn.bue.common.parameters.Parameters;
 import com.bbn.bue.common.symbols.Symbol;
+import com.bbn.bue.common.symbols.SymbolUtils;
 import com.bbn.kbp.events2014.AnswerKey;
 import com.bbn.kbp.events2014.SystemOutput;
 import com.bbn.kbp.events2014.io.AnnotationStore;
 import com.bbn.kbp.events2014.io.AssessmentSpecFormats;
 import com.bbn.kbp.events2014.io.SystemOutputStore;
 import com.bbn.kbp.events2014.transformers.KeepBestJustificationOnly;
-import com.google.common.base.Function;
-import com.google.common.base.Functions;
+import com.google.common.base.*;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+
+import static com.google.common.base.Predicates.in;
+import static com.google.common.collect.Iterables.filter;
 
 public final class ImportSystemOutputToAnnotationStore {
     private static final Logger log =
@@ -48,16 +53,12 @@ public final class ImportSystemOutputToAnnotationStore {
             final Parameters params = Parameters.loadSerifStyle(new File(argv[0]));
             log.info(params.dump());
 
-            final Function<SystemOutput, SystemOutput> filter;
-            if (params.getBoolean("importOnlyBestAnswers")) {
-                filter = KeepBestJustificationOnly.create();
-                log.info("Importing only responses the scorer would select");
-            } else {
-                filter = Functions.identity();
-                log.info("Importing all responses");
-            }
+            final Function<SystemOutput, SystemOutput> filter = getSystemOutputFilter(params);
+            final Predicate<Symbol> docIdFilter = getDocIdFilter(params);
+
             importSystemOutputToAnnotationStore(params.getExistingDirectory("systemOutput"),
                     params.getCreatableDirectory("annotationStore"), filter,
+                    docIdFilter,
                     params.getEnum("system.fileFormat", AssessmentSpecFormats.Format.class),
                     params.getEnum("annStore.fileFormat", AssessmentSpecFormats.Format.class));
         } else {
@@ -65,9 +66,35 @@ public final class ImportSystemOutputToAnnotationStore {
         }
     }
 
+    private static Function<SystemOutput, SystemOutput> getSystemOutputFilter(Parameters params) {
+        final Function<SystemOutput, SystemOutput> filter;
+        if (params.getBoolean("importOnlyBestAnswers")) {
+            filter = KeepBestJustificationOnly.create();
+            log.info("Importing only responses the scorer would select");
+        } else {
+            filter = Functions.identity();
+            log.info("Importing all responses");
+        }
+        return filter;
+    }
+
+    private static final String RESTRICT_TO_THESE_DOCS = "restrictToTheseDocs";
+    private static Predicate<Symbol> getDocIdFilter(Parameters params) throws IOException {
+        if (params.isPresent(RESTRICT_TO_THESE_DOCS)) {
+            final File docIDsFile = params.getExistingFile(RESTRICT_TO_THESE_DOCS);
+            final ImmutableList<String> docIDsToImport = Files.asCharSource(docIDsFile,
+                    Charsets.UTF_8).readLines();
+            log.info("Restricting import to {} document IDs in {}",
+                    docIDsToImport.size(), docIDsFile);
+            return in(SymbolUtils.setFrom(docIDsToImport));
+        } else {
+            return Predicates.alwaysTrue();
+        }
+    }
+
     private static void importSystemOutputToAnnotationStore(File systemOutputDir, File annStoreDir,
-        Function<SystemOutput, SystemOutput> filter, AssessmentSpecFormats.Format systemFileFormat,
-        AssessmentSpecFormats.Format annStoreFileFormat) throws IOException
+                                                            Function<SystemOutput, SystemOutput> filter, Predicate<Symbol> docIdFilter, AssessmentSpecFormats.Format systemFileFormat,
+                                                            AssessmentSpecFormats.Format annStoreFileFormat) throws IOException
     {
         log.info("Loading system output from {}", systemOutputDir);
         final SystemOutputStore systemOutput = AssessmentSpecFormats.openSystemOutputStore(
@@ -79,7 +106,7 @@ public final class ImportSystemOutputToAnnotationStore {
                 annStoreDir, annStoreFileFormat);
 
         int totalNumAdded = 0;
-        for (final Symbol docid : systemOutput.docIDs()) {
+        for (final Symbol docid : filter(systemOutput.docIDs(), docIdFilter)) {
             log.info("Pushing responses for {} into assessment store", docid);
             final SystemOutput docOutput = filter.apply(systemOutput.read(docid));
             final int numResponseInSystemOutput = docOutput.size();
