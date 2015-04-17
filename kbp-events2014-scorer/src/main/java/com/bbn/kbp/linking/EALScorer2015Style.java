5812e3c227b3616abc56209769ae551c8855ee93
Ryan Gabbard <rgabbard@bbn.com>
2015-04-17 09:26:00 -0400

    Disables overly conservative sanity check.
    We checked that the reference linking never changed when filtered against the system's linkable responses.  However, in some cases the system's linkable responses could have had additional scoring-time filtering (e.g. if there is a Life.Injure with a matching correct Life.Die, we delete it) which could create legitimate mismatches.

--- kbp-events2014-scorer/src/main/java/com/bbn/kbp/linking/EALScorer2015Style.java
+++ kbp-events2014-scorer/src/main/java/com/bbn/kbp/linking/EALScorer2015Style.java
@@ -185,12 +185,15 @@ public final class EALScorer2015Style {
     final EventArgumentLinking filteredReferenceArgumentLinking = referenceArgumentLinking
         .filteredCopy(in(linkableEquivalenceClasses));
 
-    if (!referenceArgumentLinking.equals(filteredReferenceArgumentLinking)) {
+    // We disable this check because the scorer may have done additional filtering
+    // that was not in place in the assessment tool (e.g. remove Life.Injures for correct
+    //Life.Dies).
+    /*if (!referenceArgumentLinking.equals(filteredReferenceArgumentLinking)) {
       throw new RuntimeException(
           "Filtering the reference linking should have had no effect, but it deleted " +
               Sets.difference(referenceArgumentLinking.allLinkedEquivalenceClasses(),
                   filteredReferenceArgumentLinking.allLinkedEquivalenceClasses()));
-    }
+    }*/
 
     final EventArgumentLinking filteredSystemArgumentLinking = systemArgumentLinking
         .filteredCopy(in(linkableEquivalenceClasses));
