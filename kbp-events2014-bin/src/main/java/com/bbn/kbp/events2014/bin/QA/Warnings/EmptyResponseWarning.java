551d36ac9367b9539359aa259ec1c5a1c89a0299
Jay DeYoung <jdeyoung@bbn.com>
2015-06-04 18:06:10 -0400

    numbers felt left out

--- kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/QA/Warnings/EmptyResponseWarning.java
+++ kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/QA/Warnings/EmptyResponseWarning.java
@@ -22,7 +22,7 @@ public final class EmptyResponseWarning implements WarningRule {
     final ImmutableSetMultimap.Builder<Response, Warning> result = ImmutableSetMultimap.builder();
     for(final Response r: Iterables.transform(answerKey.annotatedResponses(), AssessedResponse.Response)) {
       final String trimmed = r.canonicalArgument().string().trim();
-      final String noPunct = trimmed.replaceAll("[^A-Za-z]", "");
+      final String noPunct = trimmed.replaceAll("[^A-Za-z0-9]", "");
       if(noPunct.trim().isEmpty()) {
         result.put(r, new Warning(String.format("%s appears to be garbage or empty", trimmed),
             Warning.SEVERITY.MINIOR));
