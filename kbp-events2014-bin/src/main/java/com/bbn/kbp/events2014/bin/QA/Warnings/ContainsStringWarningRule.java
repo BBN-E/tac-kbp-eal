2a21a639a94b1f3ec3f0ca63456109214bfecabe
Ryan Gabbard <rgabbard@bbn.com>
2015-06-05 11:35:46 -0400

    Code review cleanup
    - use a splitter
    - clarify

--- kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/QA/Warnings/ContainsStringWarningRule.java
+++ kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/QA/Warnings/ContainsStringWarningRule.java
@@ -4,6 +4,7 @@ import com.bbn.kbp.events2014.AnswerKey;
 import com.bbn.kbp.events2014.AssessedResponse;
 import com.bbn.kbp.events2014.Response;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.Iterables;
@@ -12,8 +13,6 @@ import com.google.common.collect.Sets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Set;
 
 /**
  * Created by jdeyoung on 6/4/15.
@@ -45,11 +44,12 @@ abstract class ContainsStringWarningRule implements WarningRule {
     return warnings.build();
   }
 
+  private static final Splitter WHITESPACE_SPLITTER = Splitter.on("\\s+").omitEmptyStrings().trimResults();
+
   protected boolean apply(final Response input) {
-    Set<String> inputParts =
-        Sets.newHashSet(input.canonicalArgument().string().trim().toLowerCase().split(
-            "\\s+"));
-    inputParts.retainAll(verboten);
-    return inputParts.size() > 0;
+    final ImmutableSet<String> words = ImmutableSet
+        .copyOf(WHITESPACE_SPLITTER.split(input.canonicalArgument().string().toLowerCase()));
+    final Sets.SetView<String> forbiddenWords = Sets.intersection(words, verboten);
+    return !forbiddenWords.isEmpty();
   }
 }
