519f80bd497920d0937b233c29fb1c59b93089dc
Jay DeYoung <jdeyoung@bbn.com>
2015-06-08 13:12:07 -0400

    fix generated table to have param types without different group clarifications e.g. Person[1]

--- kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/QA/Warnings/ConflictingTypeWarningRule.java
+++ kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/QA/Warnings/ConflictingTypeWarningRule.java
@@ -2,6 +2,7 @@ package com.bbn.kbp.events2014.bin.QA.Warnings;
 
 import com.bbn.bue.common.StringUtils;
 import com.bbn.bue.common.annotations.MoveToBUECommon;
+import com.bbn.bue.common.collections.TableUtils;
 import com.bbn.bue.common.files.FileUtils;
 import com.bbn.bue.common.symbols.Symbol;
 import com.bbn.kbp.events2014.Response;
@@ -9,6 +10,7 @@ import com.bbn.kbp.events2014.TypeRoleFillerRealis;
 import com.bbn.kbp.events2014.bin.QA.AssessmentQA;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableTable;
@@ -24,6 +26,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by jdeyoung on 6/4/15.
@@ -46,19 +50,21 @@ public class ConflictingTypeWarningRule extends OverlapWarningRule {
   }
 
   @Override
-  protected Multimap<? extends Response, ? extends Warning> findOverlap(
+  protected Multimap<Response, Warning> findOverlap(
       final TypeRoleFillerRealis fst, final Iterable<Response> first,
       final TypeRoleFillerRealis snd, final Iterable<Response> second) {
     final ImmutableMultimap.Builder<Response, Warning> result =
         ImmutableMultimap.builder();
     for (final Response a : first) {
       final Set<Symbol> possibleFillerTypesForFirst = eventToRoleToFillerType.get(a.type(), a.role());
+      checkNotNull(possibleFillerTypesForFirst);
       for (final Response b : second) {
         if (a.equals(b) || b.canonicalArgument().string().trim().isEmpty()) {
           continue;
         }
         if (a.canonicalArgument().equals(b.canonicalArgument())) {
           final Set<Symbol> possibleFillerTypesForSecond = eventToRoleToFillerType.get(b.type(), b.role());
+          checkNotNull(possibleFillerTypesForSecond);
           final boolean noRolesPossibleForBoth =
               Sets.intersection(possibleFillerTypesForFirst, possibleFillerTypesForSecond).isEmpty();
           if (noRolesPossibleForBoth) {
@@ -84,15 +90,23 @@ public class ConflictingTypeWarningRule extends OverlapWarningRule {
         Files.asCharSource(argsFile, Charsets.UTF_8));
 
     final ImmutableTable<Symbol, Symbol, ImmutableSet<Symbol>> eventToRoleToFillerType =
-        composeToTableOfSets(roleToTypes, eventTypesToRoles);
+        TableUtils.columnTransformerByKeyOnly(composeToTableOfSets(eventTypesToRoles, roleToTypes),
+            new Function<Symbol, Symbol>() {
+              @Override
+              public Symbol apply(final Symbol input) {
+                String key = input.asString().replaceAll("\\[.\\]", "");
+                return Symbol.from(key);
+              }
+            });
 
     log.info("Role to type mapping: {}",
         StringUtils.NewlineJoiner.withKeyValueSeparator(" -> ").join(roleToTypes.asMap()));
-
+    log.info("type to concrete type mapping: {}",
+        StringUtils.NewlineJoiner.withKeyValueSeparator(" -> ").join(eventTypesToRoles.asMap()));
     for (Symbol r : eventToRoleToFillerType.rowKeySet()) {
       for (Symbol c : eventToRoleToFillerType.columnKeySet()) {
         if (c != null) {
-          log.info("{}.{}: {}", r, c, eventToRoleToFillerType.get(r, c));
+          log.info("t: {}.{}: {}", r, c, eventToRoleToFillerType.get(r, c));
         }
       }
     }
@@ -108,6 +122,7 @@ public class ConflictingTypeWarningRule extends OverlapWarningRule {
     for (final Map.Entry<R, C> firstEntry : first.entries()) {
       final R rowKey = firstEntry.getKey();
       final C colKey = firstEntry.getValue();
+      log.info("{},{} -> {}", rowKey, colKey, second.get(colKey));
 
       ret.put(rowKey, colKey, ImmutableSet.copyOf(second.get(colKey)));
     }
