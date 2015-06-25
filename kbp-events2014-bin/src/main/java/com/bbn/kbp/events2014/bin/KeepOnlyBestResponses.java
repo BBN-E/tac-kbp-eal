586a2c99c81b6c549690a5f84a64fe9843ca1ea0
Ryan Gabbard <rgabbard@bbn.com>
2015-06-25 16:12:49 -0400

    Revert "While running keepBest, be aware of ResponseLinking. Apply keepBest on each ResponseSet separately."
    This reverts commit ce1a9630a6297000e5977ca3cda72142637c6a6d.

--- kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/KeepOnlyBestResponses.java
+++ kbp-events2014-bin/src/main/java/com/bbn/kbp/events2014/bin/KeepOnlyBestResponses.java
@@ -2,16 +2,13 @@ package com.bbn.kbp.events2014.bin;
 
 import com.bbn.bue.common.parameters.Parameters;
 import com.bbn.bue.common.symbols.Symbol;
-import com.bbn.kbp.events2014.ResponseLinking;
 import com.bbn.kbp.events2014.SystemOutput;
 import com.bbn.kbp.events2014.io.AssessmentSpecFormats;
-import com.bbn.kbp.events2014.io.LinkingSpecFormats;
-import com.bbn.kbp.events2014.io.LinkingStore;
 import com.bbn.kbp.events2014.io.SystemOutputStore;
 import com.bbn.kbp.events2014.transformers.KeepBestJustificationOnly;
 import com.bbn.kbp.events2014.transformers.ProbableInferenceCases;
+
 import com.google.common.base.Function;
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 
 import org.slf4j.Logger;
@@ -45,7 +42,7 @@ public final class KeepOnlyBestResponses {
     final File inputStoreLocation = params.getExistingDirectory("inputStore");
     final File outputStoreLocation = params.getCreatableDirectory("outputStore");
     final boolean keepInferenceCases = params.getBoolean("keepInferenceCases");
-    
+
     final AssessmentSpecFormats.Format fileFormat =
         params.getEnum("fileFormat", AssessmentSpecFormats.Format.class);
     final SystemOutputStore sourceStore =
@@ -56,36 +53,11 @@ public final class KeepOnlyBestResponses {
     final Function<SystemOutput, SystemOutput> probablyInferenceFilter =
         ProbableInferenceCases.createKeepingMultisentenceJustifications();
 
-    final Optional<File> linkingStoreLocation = params.getOptionalExistingDirectory("linkingStore");
-    final Optional<LinkingStore> linkingStore;
-    if(linkingStoreLocation.isPresent()) {
-      linkingStore = Optional.of(LinkingSpecFormats.openOrCreateLinkingStore(linkingStoreLocation.get()));
-    }
-    else {
-      linkingStore = Optional.absent();
-    }
-    
     log.info("Source store has {} documents", sourceStore.docIDs().size());
     for (final Symbol docID : sourceStore.docIDs()) {
       final SystemOutput original = sourceStore.read(docID);
       final SystemOutput probableInferenceCases = probablyInferenceFilter.apply(original);
-      
-      final Optional<ResponseLinking> responseLinking;
-      if(linkingStore.isPresent()) {
-        responseLinking = linkingStore.get().read(original);
-      }
-      else {
-        responseLinking = Optional.absent();
-      }
-      
-      final SystemOutput filtered;
-      if(responseLinking.isPresent()) {
-        filtered = ((KeepBestJustificationOnly)bestJustFilter).apply(original, responseLinking.get());
-      }
-      else {
-        filtered = bestJustFilter.apply(original);
-      }
-      
+      final SystemOutput filtered = bestJustFilter.apply(original);
       int numFiltered = original.size() - filtered.size();
 
       final SystemOutput toOutput;
@@ -112,10 +84,6 @@ public final class KeepOnlyBestResponses {
 
     sourceStore.close();
     destStore.close();
-    
-    if(linkingStore.isPresent()) {
-      linkingStore.get().close();
-    }
   }
 
 
