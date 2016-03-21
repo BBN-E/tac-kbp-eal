06348c717ff59add4ce9e97b3a155b5929d3b07d
Jay DeYoung <jdeyoung@bbn.com>
2016-03-21 11:47:10 -0400

    add sorting and reading queries from a file

--- kbp-events2014/src/main/java/com/bbn/kbp/events2014/io/QueryStore2016.java
+++ kbp-events2014/src/main/java/com/bbn/kbp/events2014/io/QueryStore2016.java
@@ -1,6 +1,7 @@
 package com.bbn.kbp.events2014.io;
 
 import com.bbn.bue.common.symbols.Symbol;
+import com.bbn.kbp.events2014.AssessedQuery2016;
 import com.bbn.kbp.events2014.CharOffsetSpan;
 import com.bbn.kbp.events2014.Query2016;
 import com.bbn.kbp.events2014.QueryAssessment;
@@ -9,69 +10,72 @@ import com.google.common.base.Charsets;
 import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Ordering;
+import com.google.common.collect.TreeMultimap;
 import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Nullable;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 
-public class QueryStore2016 implements QueryStore<Query2016> {
+public final class QueryStore2016 {
 
   private final static Splitter commaSplitter = Splitter.on(",");
+  private final static Splitter dashSplitter = Splitter.on("-");
 
-  // TODO should this be immutable? probably not
-  private final ImmutableSet<Query2016> queries;
-  private final ImmutableMap<Query2016, String> metadata;
-  private final ImmutableMultimap<Symbol, Query2016> bySystemID;
-  private final ImmutableMultimap<Symbol, Query2016> byDocID;
-  private final ImmutableMap<Symbol, Query2016> byQueryID;
+  private final Set<Query2016> queries;
+  private final Map<Query2016, String> metadata;
+  private final Multimap<Symbol, Query2016> bySystemID;
+  private final Multimap<Symbol, Query2016> byDocID;
+  private final Multimap<Symbol, Query2016> byQueryID;
+  private final Map<Query2016, AssessedQuery2016> assessments;
 
 
   private QueryStore2016(final Iterable<Query2016> queries,
-      final Map<Query2016, String> metadata) {
-    this.metadata = ImmutableMap.copyOf(metadata);
+      final Map<Query2016, String> metadata, final Map<Query2016, AssessedQuery2016> assessments) {
+    this.assessments = checkNotNull(assessments);
+    this.metadata = checkNotNull(metadata);
     this.queries = ImmutableSet.copyOf(queries);
 
-    final ImmutableMap.Builder<Symbol, Query2016> byQueryID = ImmutableMap.builder();
-    final ImmutableMultimap.Builder<Symbol, Query2016> byDocID = ImmutableMultimap.builder();
-    final ImmutableMultimap.Builder<Symbol, Query2016> bySystemID = ImmutableMultimap.builder();
+    // keep these sorted
+    this.byQueryID = TreeMultimap.create(Ordering.usingToString(), by2016Ordering());
+    this.byDocID = TreeMultimap.create(Ordering.usingToString(), by2016Ordering());
+    this.bySystemID = TreeMultimap.create(Ordering.usingToString(), by2016Ordering());
 
-    for(final Query2016 q: this.queries) {
+    for (final Query2016 q : this.queries) {
       byQueryID.put(q.queryID(), q);
       byDocID.put(q.docID(), q);
       bySystemID.put(q.systemID(), q);
     }
-
-    this.byQueryID = byQueryID.build();
-    this.byDocID = byDocID.build();
-    this.bySystemID = bySystemID.build();
-
   }
 
-  @Override
   public ImmutableSet<Symbol> queryIDs() {
-    return byQueryID.keySet();
+    return ImmutableSet.copyOf(byQueryID.keySet());
   }
 
-  @Override
   public ImmutableSet<Symbol> docIDs() {
-    return byDocID.keySet();
+    return ImmutableSet.copyOf(byDocID.keySet());
   }
 
-  @Override
   public ImmutableSet<Symbol> systemIDs() {
-    return bySystemID.keySet();
+    return ImmutableSet.copyOf(bySystemID.keySet());
   }
 
-  @Override
   public ImmutableSet<Query2016> queries() {
-    return queries;
+    return ImmutableSet.copyOf(queries);
   }
 
 
@@ -81,11 +85,12 @@ public class QueryStore2016 implements QueryStore<Query2016> {
 
   public static QueryStore2016 open(final File f) throws IOException {
     final List<String> lines = Files.readLines(f, Charsets.UTF_8);
-    final ImmutableList.Builder<Query2016> queries = ImmutableList.builder();
-    final ImmutableMap.Builder<Query2016, String> metadata = ImmutableMap.builder();
+    final List<Query2016> queries = Lists.newArrayList();
+    final Map<Query2016, String> metadata = Maps.newHashMap();
+    final Map<Query2016, AssessedQuery2016> assessments = Maps.newHashMap();
     Optional<String> lastMetadata = Optional.absent();
-    for(final String line: lines) {
-      if(line.startsWith("#")) {
+    for (final String line : lines) {
+      if (line.startsWith("#")) {
         lastMetadata = Optional.of(line.trim());
       } else {
         final String[] parts = line.trim().split("\t");
@@ -93,24 +98,127 @@ public class QueryStore2016 implements QueryStore<Query2016> {
         final Symbol queryID = Symbol.from(parts[0]);
         final Symbol docID = Symbol.from(parts[1]);
         final Symbol systemID = Symbol.from(parts[2]);
-        final ImmutableList<CharOffsetSpan> spans = extractPJSpans(parts[3]);
+        final ImmutableSortedSet<CharOffsetSpan> spans = extractPJSpans(parts[3]);
         final QueryAssessment assessment = QueryAssessment.valueOf(parts[4]);
-        // TODO make a query
+        final Query2016 query = Query2016.builder().queryID(queryID).docID(docID).systemID(systemID)
+            .addAllPredicateJustifications(spans).build();
+        assessments.put(query, AssessedQuery2016.create(query, assessment));
+        queries.add(query);
+        if (lastMetadata.isPresent()) {
+          metadata.put(query, lastMetadata.get());
+          lastMetadata = Optional.absent();
+        }
       }
     }
 
-    return new QueryStore2016(queries.build(), metadata.build());
+    return new QueryStore2016(queries, metadata, assessments);
   }
 
-
-  private static ImmutableList<CharOffsetSpan> extractPJSpans(final String part) {
-    // TODO implement
-    return null;
-  }
 
   public static QueryStore2016 createEmpty() {
     // TODO should this take a file?
     return null;
+  }
+
+  private static ImmutableSortedSet<CharOffsetSpan> extractPJSpans(final String part) {
+    final ImmutableSortedSet.Builder<CharOffsetSpan> spans = ImmutableSortedSet.naturalOrder();
+    for (final String spanString : commaSplitter.split(part.trim())) {
+      final List<String> spanParts = ImmutableList.copyOf(dashSplitter.split(spanString));
+      checkState(spanParts.size() == 2,
+          "Expected two components to the span, but got " + spanParts.size());
+      final int start = Integer.parseInt(spanParts.get(0));
+      final int end = Integer.parseInt(spanParts.get(1));
+      spans.add(CharOffsetSpan.fromOffsetsOnly(start, end));
+    }
+    return spans.build();
+  }
+
+
+  private static Ordering<Query2016> byQueryID() {
+    return new Ordering<Query2016>() {
+      @Override
+      public int compare(@Nullable final Query2016 left, @Nullable final Query2016 right) {
+        checkNotNull(left);
+        checkNotNull(right);
+        return left.queryID().asString().compareTo(right.queryID().asString());
+      }
+    };
+  }
+
+  private static Ordering<Query2016> byDocID() {
+    return new Ordering<Query2016>() {
+      @Override
+      public int compare(@Nullable final Query2016 left, @Nullable final Query2016 right) {
+        checkNotNull(left);
+        checkNotNull(right);
+        return left.docID().asString().compareTo(right.docID().asString());
+      }
+    };
+  }
+
+  private static Ordering<Query2016> bySystemID() {
+    return new Ordering<Query2016>() {
+      @Override
+      public int compare(@Nullable final Query2016 left, @Nullable final Query2016 right) {
+        checkNotNull(left);
+        checkNotNull(right);
+        return left.systemID().asString().compareTo(right.systemID().asString());
+      }
+    };
+  }
+
+  private static Ordering<Query2016> byPJOffsets() {
+    return new Ordering<Query2016>() {
+      @Override
+      public int compare(@Nullable final Query2016 left, @Nullable final Query2016 right) {
+        checkNotNull(left);
+        checkNotNull(right);
+        checkArgument(left.docID() == right.docID(),
+            "Refusing to compare PJs between different documents!");
+        // iterate through comparing the PJs. These are guaranteed to be sorted by lowest index, then shortest length.
+        final List<CharOffsetSpan> leftSpans = ImmutableList.copyOf(left.predicateJustifications());
+        final List<CharOffsetSpan> rightSpans =
+            ImmutableList.copyOf(right.predicateJustifications());
+        for (int i = 0; i < leftSpans.size() && i < rightSpans.size(); i++) {
+          final int cmp = leftSpans.get(i)
+              .compareTo(rightSpans.get(i));
+          if (cmp != 0) {
+            return cmp;
+          }
+        }
+        // if everything has been the same, the shorter list appears earlier
+        if (leftSpans.size() < rightSpans.size()) {
+          return -1;
+        } else if (leftSpans.size() > rightSpans.size()) {
+          return 1;
+        }
+        return 0;
+      }
+    };
+  }
+
+  private static Ordering<Query2016> by2016Ordering() {
+    return Orderings.byQueryID.ordering().compound(Orderings.byDocID.ordering())
+        .compound(Orderings.bySystemID.ordering()).compound(Orderings.byPJOffsets.ordering());
+  }
+
+
+  enum Orderings {
+    byQueryID(QueryStore2016.<Query2016>byQueryID()),
+    byDocID(QueryStore2016.<Query2016>byDocID()),
+    bySystemID(QueryStore2016.<Query2016>bySystemID()),
+    byPJOffsets(QueryStore2016.byPJOffsets()),
+    by2016Ordering(QueryStore2016.by2016Ordering());
+
+    private final Ordering<Query2016> ordering;
+
+    Orderings(final Ordering<Query2016> ordering) {
+      this.ordering = ordering;
+    }
+
+    public final Ordering<Query2016> ordering() {
+      return ordering;
+    }
   }
 
 }
