9e32fc5284976d8d8ec3d82dfe7327b02663322e
Ryan Gabbard <rgabbard@bbn.com>
2016-03-21 17:23:02 -0400

    Immutables-ize ResponseSet

---
+++ tac-kbp-eal/src/main/java/com/bbn/kbp/events2014/_ResponseSet.java
@@ -0,0 +1,61 @@
+package com.bbn.kbp.events2014;
+
+import com.bbn.bue.common.TextGroupPublicImmutable;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Ordering;
+
+import org.immutables.value.Value;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Represents a set of responses.  The default comparison method is to do a lexicographical sort of
+ * the unique IDs of the (sorted) responses.
+ */
+@Value.Immutable
+@TextGroupPublicImmutable
+public abstract class _ResponseSet implements Comparable<ResponseSet>, Iterable<Response> {
+
+  @Value.Parameter
+  public abstract ImmutableSet<Response> responses();
+
+  public static ResponseSet from(Iterable<Response> responses) {
+    return ResponseSet.of(responses);
+  }
+
+  public static ResponseSet from(Response... responses) {
+    return from(Arrays.asList(responses));
+  }
+
+  public final ImmutableSet<Response> asSet() {
+    return responses();
+  }
+
+
+  private static Function<ResponseSet, ImmutableSet<Response>> asSetFunction() {
+    return new Function<ResponseSet, ImmutableSet<Response>>() {
+      @Override
+      public ImmutableSet<Response> apply(ResponseSet input) {
+        return input.asSet();
+      }
+    };
+  }
+
+  @Override
+  public final int compareTo(ResponseSet o) {
+    return Ordering.natural()
+        .onResultOf(Response.uniqueIdFunction())
+        .lexicographical()
+        .onResultOf(asSetFunction())
+        .compare((ResponseSet) this, o);
+  }
+
+  @Override
+  public final Iterator<Response> iterator() {
+    return asSet().iterator();
+  }
+
+}
