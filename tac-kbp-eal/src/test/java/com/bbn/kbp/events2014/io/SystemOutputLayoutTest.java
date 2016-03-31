c6eeb5077f6db97c3801084fb893a585018c708c
Jay DeYoung <jdeyoung@bbn.com>
2016-03-31 18:04:23 -0400

    update code for generating test resources

--- tac-kbp-eal/src/test/java/com/bbn/kbp/events2014/io/SystemOutputLayoutTest.java
+++ tac-kbp-eal/src/test/java/com/bbn/kbp/events2014/io/SystemOutputLayoutTest.java
@@ -2,14 +2,12 @@ package com.bbn.kbp.events2014.io;
 
 
 import com.bbn.bue.common.symbols.Symbol;
-import com.bbn.kbp.events2014.AnswerKey;
-import com.bbn.kbp.events2014.ArgumentOutput;
-import com.bbn.kbp.events2014.AssessedResponseFunctions;
+import com.bbn.kbp.events2014.CorefAnnotation;
 import com.bbn.kbp.events2014.CorpusEventFrame;
 import com.bbn.kbp.events2014.CorpusEventLinking;
 import com.bbn.kbp.events2014.DocEventFrameReference;
+import com.bbn.kbp.events2014.DocumentSystemOutput;
 import com.bbn.kbp.events2014.DocumentSystemOutput2015;
-import com.bbn.kbp.events2014.EventArgumentLinking;
 import com.bbn.kbp.events2014.Response;
 import com.bbn.kbp.events2014.ResponseFunctions;
 import com.bbn.kbp.events2014.ResponseLinking;
@@ -24,16 +22,19 @@ import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.getFirst;
 
 public final class SystemOutputLayoutTest {
 
-  final Symbol testDocID = Symbol.from("AFP_ENG_20091024.0206.fake");
+  private static final Logger log = LoggerFactory.getLogger(SystemOutputLayoutTest.class);
 
   @Test
   public void test2016LayoutReading() throws IOException {
@@ -52,67 +53,76 @@ public final class SystemOutputLayoutTest {
   }
 
   private void create2016LayoutTest() throws IOException {
+    // this must point to a directory via your filesystem, not a resource, unless you want to debug.
+    // what are you doing here anyway?
+    final File dir = new File(
+        "/nfs/e-nfs-01/home/jdeyoung/workspace/kbp-2014-event-arguments/tac-kbp-eal/src/test/resources/com/bbn/kbp/events2014/io/CorpusEventTest"
+//        SystemOutputLayoutTest.class
+//            .getResource("/com/bbn/kbp/events2014/io/CorpusEventTest/")
+//            .getFile()
+    );
+
+    final SystemOutputStore sourceLayout = SystemOutputLayout.KBP_EA_2015.open(dir);
+    final ImmutableMap.Builder<String, TypeRoleFillerRealisSet> completeTRFRMap =
+        ImmutableMap.builder();
+
+    for (final Symbol docID : sourceLayout.docIDs()) {
+      log.info("Processing {}", docID);
+      final DocumentSystemOutput answerKey = sourceLayout.read(docID);
+
+      // coref everything in the same document
+      final ImmutableSet<Response> responses = answerKey.arguments().responses();
+      final ImmutableMultimap<Symbol, Response> typeToResponse =
+          FluentIterable.from(responses).index(
+              ResponseFunctions.type());
+      final ResponseLinking.Builder responseLinkingB = ResponseLinking.builder().docID(docID);
+      for (final Symbol type : typeToResponse.keySet()) {
+        checkState(typeToResponse.get(type).size() > 0);
+        responseLinkingB.addResponseSets(ResponseSet.of(typeToResponse.get(type)));
+      }
+      final ResponseLinking responseLinking = responseLinkingB.build();
+      log.info("ResponseLinking size is {}", responseLinking.responseSets().size());
+
+      final DocumentSystemOutput2015 documentSystemOutput2015 = DocumentSystemOutput2015
+          .from(answerKey.arguments(), responseLinking);
+      sourceLayout.write(documentSystemOutput2015);
+
+      final ImmutableMultimap<TypeRoleFillerRealis, Response> trfrs = FluentIterable.from(responses)
+          .index(TypeRoleFillerRealis
+              .extractFromSystemResponse(
+                  CorefAnnotation.laxBuilder(docID).build().laxCASNormalizerFunction()));
+      final ImmutableMap.Builder<String, TypeRoleFillerRealisSet> idMapB = ImmutableMap.builder();
+      for (final TypeRoleFillerRealis trfr : trfrs.keySet()) {
+        final TypeRoleFillerRealisSet trfrSet =
+            TypeRoleFillerRealisSet.create(ImmutableSet.of(trfr));
+        idMapB.put(trfr.uniqueIdentifier(), trfrSet);
+      }
+      completeTRFRMap.putAll(idMapB.build());
+    }
+    sourceLayout.close();
+
+    // this should really point to the same place as used above
     final SystemOutputStore2016 outputStore =
         (SystemOutputStore2016) SystemOutputLayout.KBP_EA_2016
             .openOrCreate(new File(
-                this.getClass().getResource("/com/bbn/kbp/events2014/io/CorpusEventTest")
-                    .getFile()));
+                    "/nfs/e-nfs-01/home/jdeyoung/workspace/kbp-2014-event-arguments/tac-kbp-eal/src/test/resources/com/bbn/kbp/events2014/io/CorpusEventTest")
+//                this.getClass().getResource("/com/bbn/kbp/events2014/io/CorpusEventTest")
+//                    .getFile())
+            );
 
-    final File dir = new File(
-        SystemOutputLayoutTest.class.getResource("/com/bbn/kbp/events2014/io/linkingTest")
-            .getFile());
-
-    final AnswerKey answerKey =
-        AssessmentSpecFormats.openAnnotationStore(dir, AssessmentSpecFormats.Format.KBP2015)
-            .read(testDocID);
-
-    final ImmutableSet<Response> responses =
-        FluentIterable.from(answerKey.annotatedResponses())
-            .transform(AssessedResponseFunctions.response())
-            .toSet(); //output.arguments().responses();
-    final ImmutableMultimap<Symbol, Response> typeToResponse = FluentIterable.from(responses).index(
-        ResponseFunctions.type());
-    final ResponseLinking.Builder responseLinkingB = ResponseLinking.builder().docID(testDocID);
-    for (final Symbol type : typeToResponse.keySet()) {
-      responseLinkingB.addResponseSets(ResponseSet.of(typeToResponse.get(type)));
-    }
-    final ResponseLinking responseLinking = responseLinkingB.build();
-
-    final ImmutableMultimap<TypeRoleFillerRealis, Response> trfrs = FluentIterable.from(responses)
-        .index(TypeRoleFillerRealis
-            .extractFromSystemResponse(answerKey.corefAnnotation().laxCASNormalizerFunction()));
-    final ImmutableSet.Builder<TypeRoleFillerRealisSet> trfrSetsB = ImmutableSet.builder();
-    final ImmutableMap.Builder<String, TypeRoleFillerRealisSet> idMapB = ImmutableMap.builder();
-    for (final TypeRoleFillerRealis trfr : trfrs.keySet()) {
-      final TypeRoleFillerRealisSet trfrSet = TypeRoleFillerRealisSet.create(ImmutableSet.of(trfr));
-      idMapB.put(trfr.uniqueIdentifier(), trfrSet);
-      trfrSetsB.add(trfrSet);
-    }
-
-    final EventArgumentLinking eventArgumentLinking =
-        EventArgumentLinking.builder().addAllEventFrames(trfrSetsB.build())
-            .idsToEventFrames(idMapB.build()).docID(testDocID).build();
-    checkState(eventArgumentLinking.eventFrames().size() > 0);
-
-    final CorpusEventLinking.Builder corpusEventLinkingB;
-    if (outputStore.readCorpusEventFrames().corpusEventFrames().size() == 0) {
-      corpusEventLinkingB = CorpusEventLinking.builder();
-    } else {
-      corpusEventLinkingB = CorpusEventLinking.builder().from(outputStore.readCorpusEventFrames());
-    }
+    final CorpusEventLinking.Builder corpusEventLinkingB = CorpusEventLinking.builder();
     final ImmutableMap<String, TypeRoleFillerRealisSet> hopperIDs =
-        eventArgumentLinking.idsToEventFrames().get();
+        completeTRFRMap.build();
     for (final String id : hopperIDs.keySet()) {
       corpusEventLinkingB.addCorpusEventFrames(CorpusEventFrame.of(id,
-          ImmutableSet.of(DocEventFrameReference.of(testDocID, id))));
+          ImmutableSet.of(DocEventFrameReference
+              .of(checkNotNull(getFirst(hopperIDs.get(id), null)).docID(), id))));
     }
-
-    final DocumentSystemOutput2015 documentSystemOutput2015 = DocumentSystemOutput2015
-        .from(ArgumentOutput.createWithConstantScore(testDocID, answerKey.allResponses(), 1.0),
-            responseLinking);
-    outputStore.write(documentSystemOutput2015);
+    checkState(hopperIDs.size() > 0);
     outputStore.writeCorpusEventFrames(corpusEventLinkingB.build());
+
     outputStore.close();
+
 
   }
 
