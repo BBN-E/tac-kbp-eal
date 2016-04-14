84a81be35a78d3c5fc1e8b638fc90c8f39bbf2bd
Jay DeYoung <jdeyoung@bbn.com>
2016-04-14 14:38:34 -0400

    add alignment stuff for Responses to ERE *

--- tac-kbp-eal-scorer/src/main/java/com/bbn/kbp/events/ScoreKBPAgainstERE.java
+++ tac-kbp-eal-scorer/src/main/java/com/bbn/kbp/events/ScoreKBPAgainstERE.java
@@ -38,8 +38,10 @@ import com.bbn.nlp.corpora.ere.EREEntityArgument;
 import com.bbn.nlp.corpora.ere.EREEntityMention;
 import com.bbn.nlp.corpora.ere.EREEvent;
 import com.bbn.nlp.corpora.ere.EREEventMention;
+import com.bbn.nlp.corpora.ere.EREFiller;
 import com.bbn.nlp.corpora.ere.EREFillerArgument;
 import com.bbn.nlp.corpora.ere.ERELoader;
+import com.bbn.nlp.corpora.ere.ERESpan;
 import com.bbn.nlp.events.HasEventType;
 import com.bbn.nlp.events.scoring.DocLevelEventArg;
 import com.bbn.nlp.parsing.HeadFinders;
@@ -65,6 +67,7 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Random;
 
 import javax.annotation.Nullable;
@@ -73,6 +76,7 @@ import static com.bbn.bue.common.evaluation.InspectorTreeDSL.inspect;
 import static com.bbn.bue.common.evaluation.InspectorTreeDSL.transformLeft;
 import static com.bbn.bue.common.evaluation.InspectorTreeDSL.transformRight;
 import static com.bbn.bue.common.evaluation.InspectorTreeDSL.transformed;
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Iterables.filter;
@@ -373,7 +377,8 @@ public final class ScoreKBPAgainstERE {
                 baseFillerOffsets, response, parseString);
             mentionAlignmentFailures.add(errKey(response));
           } else {
-            log.info("Failed to align base filler with offsets {} to an ERE mention for response {}",
+            log.info(
+                "Failed to align base filler with offsets {} to an ERE mention for response {}",
                 baseFillerOffsets, response);
             mentionAlignmentFailures.add(errKey(response));
           }
@@ -382,7 +387,7 @@ public final class ScoreKBPAgainstERE {
               + " using the first one found with a matching type!");
         }
 
-
+        // TODO handle fillers (e.g. times)
         // TODO match this using the type instead of first entity
         final EREEntity matchingEntity = Iterables.getFirst(candidateEntities, null);
         if (matchingEntity != null) {
@@ -420,6 +425,7 @@ final class EREAligner {
   private final ImmutableMultimap<Range<CharOffset>, EREEntityMention>
       exactRangeToEntityMentionHead;
   private final ImmutableOverlappingRangeSet<CharOffset> exactHeadRange;
+  private final ImmutableMultimap<Range<CharOffset>, EREFiller> rangeToFiller;
   private final Optional<CoreNLPDocument> coreNLPDoc;
 
   private EREAligner(final boolean relaxUsingCORENLP,
@@ -435,6 +441,7 @@ final class EREAligner {
     this.exactHeadRange =
         ImmutableOverlappingRangeSet.create(exactRangeToEntityMentionHead.keySet());
     this.coreNLPDoc = coreNLPDocument;
+    this.rangeToFiller = rangeToFiller(ereDoc);
   }
 
   static EREAligner create(final boolean relaxUsingCORENLP,
@@ -461,33 +468,41 @@ final class EREAligner {
     // add the candidate mentions whose head aligns exactly with the base filler offsets
     candidateMentionsB.addAll(exactRangeToEntityMentionHead.get(baseFillerOffsets.asRange()));
     if (relaxUsingCORENLP) {
-      final Optional<CoreNLPSentence> sent =
-          coreNLPDoc.get().firstSentenceContaining(baseFillerOffsets);
-      if (sent.isPresent()) {
-        final Optional<CoreNLPParseNode> node =
-            sent.get().nodeForOffsets(baseFillerOffsets);
-        if (node.isPresent()) {
-          final Optional<CoreNLPParseNode> terminalHead = node.get().terminalHead();
-          if (terminalHead.isPresent()) {
-            final Range<CharOffset> coreNLPHeadRange = terminalHead.get().span().asRange();
-            if (useExactMatchForCoreNLPRelaxation) {
-              // add the candidate entity mentions whose heads exactly match the CoreNLPHead
-              candidateMentionsB.addAll(exactRangeToEntityMentionHead.get(coreNLPHeadRange));
-            } else {
-              // add the candidate entity mentions whose heads contain the CoreNLPHead
-              candidateMentionsB.addAll(MultimapUtils.getAll(exactRangeToEntityMentionHead,
-                  exactHeadRange.rangesContaining(coreNLPHeadRange)));
-              // add the candidate entity mentions whose heads are contained in the CoreNLPHead
-              candidateMentionsB.addAll(MultimapUtils.getAll(exactRangeToEntityMentionHead,
-                  exactHeadRange.rangesContained(coreNLPHeadRange)));
-            }
-          }
+      final Optional<Range<CharOffset>> coreNLPHeadRange = getCoreNLPHead(baseFillerOffsets);
+      if (coreNLPHeadRange.isPresent()) {
+        if (useExactMatchForCoreNLPRelaxation) {
+          // add the candidate entity mentions whose heads exactly match the CoreNLPHead
+          candidateMentionsB.addAll(exactRangeToEntityMentionHead.get(coreNLPHeadRange.get()));
+        } else {
+          // add the candidate entity mentions whose heads contain the CoreNLPHead
+          candidateMentionsB.addAll(MultimapUtils.getAll(exactRangeToEntityMentionHead,
+              exactHeadRange.rangesContaining(coreNLPHeadRange.get())));
+          // add the candidate entity mentions whose heads are contained in the CoreNLPHead
+          candidateMentionsB.addAll(MultimapUtils.getAll(exactRangeToEntityMentionHead,
+              exactHeadRange.rangesContained(coreNLPHeadRange.get())));
         }
       }
     }
 
     final ImmutableSet<EREEntityMention> candidateMentions = candidateMentionsB.build();
     return getCandidateEntitiesFromMentions(ereDoc, candidateMentions);
+  }
+
+  private Optional<Range<CharOffset>> getCoreNLPHead(final OffsetRange<CharOffset> offsets) {
+    final Optional<CoreNLPSentence> sent =
+        coreNLPDoc.get().firstSentenceContaining(offsets);
+    if (sent.isPresent()) {
+      final Optional<CoreNLPParseNode> node =
+          sent.get().nodeForOffsets(offsets);
+      if (node.isPresent()) {
+        final Optional<CoreNLPParseNode> terminalHead = node.get().terminalHead();
+        if (terminalHead.isPresent()) {
+          final Range<CharOffset> coreNLPHeadRange = terminalHead.get().span().asRange();
+          return Optional.of(coreNLPHeadRange);
+        }
+      }
+    }
+    return Optional.absent();
   }
 
   private static ImmutableSet<EREEntity> getCandidateEntitiesFromMentions(final EREDocument doc,
@@ -518,6 +533,20 @@ final class EREAligner {
     return ret.build();
   }
 
+  private static ImmutableMultimap<Range<CharOffset>, EREFiller> rangeToFiller(
+      final EREDocument ereDoc) {
+    final ImmutableMultimap.Builder<Range<CharOffset>, EREFiller> ret =
+        ImmutableMultimap.builder();
+    for (final EREFiller f : ereDoc.getFillers()) {
+      // TODO check for off-by-one error since we use [closed,closed] offsets
+      final Range<CharOffset> r =
+          CharOffsetSpan.fromOffsetsOnly(f.getExtent().getStart(), f.getExtent().getEnd())
+              .asCharOffsetRange().asRange();
+      ret.put(r, f);
+    }
+    return ret.build();
+  }
+
   private static final Function<EREEntityMention, Optional<Range<CharOffset>>> spanExtractor =
       new Function<EREEntityMention, Optional<Range<CharOffset>>>() {
         @Nullable
@@ -525,6 +554,7 @@ final class EREAligner {
         public Optional<Range<CharOffset>> apply(
             @Nullable final EREEntityMention ereEntityMention) {
           checkNotNull(ereEntityMention);
+          // TODO check for off-by-one error since we use [closed,closed] offsets
           return Optional
               .of(CharOffsetSpan.fromOffsetsOnly(ereEntityMention.getExtent().getStart(),
                   ereEntityMention.getExtent().getEnd()).asCharOffsetRange().asRange());
@@ -539,6 +569,7 @@ final class EREAligner {
             @Nullable final EREEntityMention ereEntityMention) {
           checkNotNull(ereEntityMention);
           if (ereEntityMention.getHead().isPresent()) {
+            // TODO check for off-by-one error since we use [closed,closed] offsets
             return Optional.of(CharOffsetSpan
                 .fromOffsetsOnly(ereEntityMention.getHead().get().getStart(),
                     ereEntityMention.getHead().get().getEnd()).asCharOffsetRange().asRange());
@@ -547,6 +578,109 @@ final class EREAligner {
           }
         }
       };
+
+  private boolean spanMatches(final ERESpan es, final CharOffsetSpan cs) {
+    // TODO check for off-by-one error since we use [closed,closed] offsets
+    final Range<CharOffset> esRange =
+        CharOffsetSpan.fromOffsetsOnly(es.getStart(), es.getEnd()).asCharOffsetRange().asRange();
+    final Range<CharOffset> csRange = cs.asCharOffsetRange().asRange();
+    if (esRange.equals(csRange)) {
+      return true;
+    }
+    if (!useExactMatchForCoreNLPRelaxation && esRange.encloses(csRange)) {
+      return true;
+    }
+    if (relaxUsingCORENLP) {
+      final Optional<Range<CharOffset>> headRange = getCoreNLPHead(cs.asCharOffsetRange());
+      if (headRange.isPresent()) {
+        if (esRange.equals(headRange.get())) {
+          return true;
+        }
+        // TODO do we want this second .encloses?
+        if (!useExactMatchForCoreNLPRelaxation && (esRange.encloses(headRange.get()) || headRange
+            .get().encloses(esRange))) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  ImmutableSet<ResolvedEREEventMentionArg> argsForResponse(final Response r) {
+    final ImmutableSet.Builder<ResolvedEREEventMentionArg> ret = ImmutableSet.builder();
+    // TODO what to do about generic events? This level handling probably doesn't belong here...
+    // TODO what to do about mismatched response and EREArgs?
+    for (final EREEvent ev : ereDoc.getEvents()) {
+      for (final EREEventMention evm : ev.getEventMentions()) {
+        // TODO check type
+        for (final EREArgument arg : evm.getArguments()) {
+          EREFiller f = null;
+          EREEntityMention em = null;
+          final Optional<ERESpan> head;
+          final ERESpan extent;
+          if (arg instanceof EREFillerArgument) {
+            f = ((EREFillerArgument) arg).filler();
+            head = Optional.of(f.getExtent());
+            extent = f.getExtent();
+          } else if (arg instanceof EREEntityArgument) {
+            em = ((EREEntityArgument) arg).entityMention();
+            head = em.getHead();
+            extent = em.getExtent();
+          } else {
+            throw new RuntimeException("Unknown ERE arg type " + arg.getClass());
+          }
+
+          if (spanMatches(extent, r.baseFiller()) || (head.isPresent() && spanMatches(head.get(),
+              r.baseFiller()))) {
+            ret.add(ResolvedEREEventMentionArg
+                .create(evm, Optional.fromNullable(f), Optional.fromNullable(em)));
+          }
+        }
+      }
+    }
+    return ret.build();
+  }
+
+}
+
+final class ResolvedEREEventMentionArg {
+
+  final EREEventMention ereEventMention;
+  final Optional<EREFiller> ereFiller;
+  final Optional<EREEntityMention> entityMention;
+
+  private ResolvedEREEventMentionArg(final EREEventMention ereEventMention,
+      final Optional<EREFiller> ereFiller, final Optional<EREEntityMention> entityMention) {
+    this.ereEventMention = checkNotNull(ereEventMention);
+    this.ereFiller = checkNotNull(ereFiller);
+    this.entityMention = checkNotNull(entityMention);
+    checkArgument(ereFiller.isPresent() || entityMention.isPresent(),
+        "Either a filler or an entity mention must be present!");
+  }
+
+  static ResolvedEREEventMentionArg create(final EREEventMention ereEventMention,
+      final Optional<EREFiller> ereFiller, final Optional<EREEntityMention> entityMention) {
+    return new ResolvedEREEventMentionArg(ereEventMention, ereFiller, entityMention);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final ResolvedEREEventMentionArg that = (ResolvedEREEventMentionArg) o;
+    return Objects.equals(ereEventMention, that.ereEventMention) &&
+        Objects.equals(ereFiller, that.ereFiller) &&
+        Objects.equals(entityMention, that.entityMention);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(ereEventMention, ereFiller, entityMention);
+  }
 }
 
 final class EREDocAndResponses {
